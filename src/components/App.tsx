2d1b450bfce18edaf799e2f8aed64a25f616156c
Pavel Zharov <pavel@prna.ru>
2021-09-07 21:32:30 +0700

    feat: :sparkles: add section with product images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,19 @@ import Header from './Header';
 import '../styles/App.css';
 import Comparsion from './Comparsion';
 import Main from './Main';
+import initialProoducts from '../data/smartphones.json';
+import { useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState(initialProoducts || null);
+
   return (
     <div className="page">
       <Header title="Каталог" />
       <Main>
         <Comparsion
-        title="Смартфоны"/>
+        title="Смартфоны"
+        products={products} />
       </Main>
     </div>
   );
--- src/components/Comparsion.tsx
+++ src/components/Comparsion.tsx
@@ -6,25 +6,30 @@ function Comparsion(props: any) {
 
   const handleClick = (e: any) => {
     setQuantity(Number(e.target.textContent));
-    console.log(quantity);
   }
   return (
     <section className="compare section">
       <div className="section__container compare__container">
         <h1 className="compare__title">{props.title}</h1>
-        <div className="compare__quantity">
-          <p className="compare__quantity-text">
-            Отобразить товары:
-            <button className={`compare__button ${quantity === 2 ? 'compare__button_active' : ''}`} onClick={handleClick}>2</button>
-            <button className={`compare__button ${quantity === 3 ? 'compare__button_active' : ''}`} onClick={handleClick}>3</button>
-            <button className={`compare__button ${quantity === 4 ? 'compare__button_active' : ''}`} onClick={handleClick}>4</button>
-            <button className={`compare__button ${quantity === 5 ? 'compare__button_active' : ''}`} onClick={handleClick}>5</button>
-            <button className={`compare__button ${quantity === 6 ? 'compare__button_active' : ''}`} onClick={handleClick}>6</button>
-          </p>
-        </div>
+        <p className="compare__quantity compare__text">
+          Отобразить товары:
+          <button className={`compare__button ${quantity === 2 ? 'compare__button_active' : ''}`} onClick={handleClick}>2</button>
+          <button className={`compare__button ${quantity === 3 ? 'compare__button_active' : ''}`} onClick={handleClick}>3</button>
+          <button className={`compare__button ${quantity === 4 ? 'compare__button_active' : ''}`} onClick={handleClick}>4</button>
+          <button className={`compare__button ${quantity === 5 ? 'compare__button_active' : ''}`} onClick={handleClick}>5</button>
+          <button className={`compare__button ${quantity === 6 ? 'compare__button_active' : ''}`} onClick={handleClick}>6</button>
+        </p>
         <div className="compare__table">
           <div className="compare__differences">
-            <input type="checkbox" name="differences" id="differences" />
+            <label htmlFor="differences" className="compare__text">
+              <input type="checkbox" name="differences" id="differences" />
+              Показать различия
+            </label>
+          </div>
+          <div className="compare__images">
+            {
+            props.products.map((product: any, i: number) => (i < quantity) ? <img src={`/images/${product.image}`} alt="ffd"/> : '')
+            }
           </div>
         </div>
 
